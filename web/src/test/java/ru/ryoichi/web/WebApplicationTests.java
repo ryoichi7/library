4ed57ee3129fe0cc593b031f9bd091130bb6ecb1
ryoichi7 <ryoichidoto@gmail.com>
2024-04-25 12:45:18 +0300

    initial commit

---
+++ web/src/test/java/ru/ryoichi/web/WebApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.ryoichi.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
